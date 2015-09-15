7fc4dbbe492540b9d0795a15b6e640726bb3959c
Sam T <sthompson@digitalpulp.com>
2015-09-15 14:00:09 -0400

    fix namespace

--- src/Venue.php
+++ src/Venue.php
@@ -1,19 +1,25 @@
 <?php
-// src/Venue.php
+
+namespace PF;
+
 use Doctrine\Common\Collections\ArrayCollection;
 
 /**
  * @Entity @Table(name="products")
  **/
 class Venue {
+
   /** @Id @Column(type="integer") @GeneratedValue **/
   protected $id;
   /** @Column(type="string") **/
   protected $name;
+
   protected $machines;
+  protected $comments;
 
   public function __construct() {
     $this->machines = new ArrayCollection();
+    $this->comments = new ArrayCollection();
   }
 
   public function getId()
