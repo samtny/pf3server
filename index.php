570a2410a3eadbc7f4d84ecd71543ebfdd2893f1
Sam T <sthompson@digitalpulp.com>
2016-03-21 12:16:06 -0400

    tidy routes

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 require_once 'bootstrap.php';
 
-$app->group('/venue', function () use ($app, $entityManager) {
+$app->group('/venue', function () use ($app, $entityManager, $venueDeserializer) {
   $app->get('/search', function () use ($app, $entityManager) {
     $venuesIterator = $entityManager->getRepository('\PF\Venue')->getVenues($app->request());
 
@@ -24,22 +24,22 @@ $app->group('/venue', function () use ($app, $entityManager) {
 
     $app->responseData = array('venue' => $venue);
   });
-});
 
-$app->post('/venue', function () use ($app, $entityManager, $venueDeserializer) {
-  $venue = $venueDeserializer->deserialize($app->request->getBody());
+  $app->post('/', function () use ($app, $entityManager, $venueDeserializer) {
+    $venue = $venueDeserializer->deserialize($app->request->getBody());
 
-  $is_new_venue = empty($venue->getId());
+    $is_new_venue = empty($venue->getId());
 
-  try {
-    $entityManager->persist($venue);
+    try {
+      $entityManager->persist($venue);
 
-    $entityManager->flush();
+      $entityManager->flush();
 
-    $app->responseMessage = ($is_new_venue ? 'Created Venue with ID ' : 'Updated Venue with ID ') . $venue->getId();
-  } catch (\Doctrine\ORM\EntityNotFoundException $e) {
-    $app->notFound();
-  }
+      $app->responseMessage = ($is_new_venue ? 'Created Venue with ID ' : 'Updated Venue with ID ') . $venue->getId();
+    } catch (\Doctrine\ORM\EntityNotFoundException $e) {
+      $app->notFound();
+    }
+  });
 });
 
 $app->group('/game', function () use ($app, $entityManager) {
