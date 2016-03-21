ddbf85ca09778499f87a3923e5354b24f1b132b7
Sam T <sthompson@digitalpulp.com>
2016-03-21 11:19:46 -0400

    move cache settings to 'production'

--- bootstrap.php
+++ bootstrap.php
@@ -12,32 +12,7 @@ $app = new \PF\Slim\PinfinderApp(
   )
 );
 
-$app->view()->parserOptions = array(
-  'autoescape' => false,
-);
-
-$app->notFound(function () use ($app) {
-  $app->render('404.html');
-});
-
-$app->configureMode('development', function () use ($app) {
-  $app->config(array(
-    'cookies.lifetime' => 'Never',
-    'debug' => true,
-  ));
-});
-
-$app->configureMode('production', function () use ($app) {
-  $app->config(array(
-    'cookies.lifetime' => '2 Hours',
-    'debug' => false,
-  ));
-});
-
 $config = Setup::createYAMLMetadataConfiguration(array(__DIR__ . '/src/PF/Doctrine'), $app->getMode() === 'development', null, null);
-
-//$config->setQueryCacheImpl(new \Doctrine\Common\Cache\ApcCache());
-//$config->setMetadataCacheImpl(new \Doctrine\Common\Cache\ApcCache());
 
 $config->addCustomNumericFunction('sin', '\DoctrineExtensions\Query\Mysql\Sin');
 $config->addCustomNumericFunction('cos', '\DoctrineExtensions\Query\Mysql\Cos');
@@ -72,15 +47,43 @@ $object_constructor = new \JMS\Serializer\Construction\DoctrineObjectConstructor
 
 $initialized_object_constructor = new \PF\Serializer\InitializedObjectConstructor($object_constructor);
 
-$serializer = JMS\Serializer\SerializerBuilder::create()
+$serializer_builder = JMS\Serializer\SerializerBuilder::create()
   ->setMetadataDirs(array('PF' => __DIR__ . '/src/PF/Serializer'))
   ->setDebug($app->getMode() === 'development')
-  ->setObjectConstructor($initialized_object_constructor)
-  //->setCacheDir('/tmp')
-  ->build();
+  ->setObjectConstructor($initialized_object_constructor);
+
+
+$app->configureMode('development', function () use ($app) {
+  $app->config(array(
+    'cookies.lifetime' => 'Never',
+    'debug' => true,
+  ));
+});
+
+$app->configureMode('production', function () use ($app, $config, $serializer_builder) {
+  $app->config(array(
+    'cookies.lifetime' => '2 Hours',
+    'debug' => false,
+  ));
+
+  $config->setQueryCacheImpl(new \Doctrine\Common\Cache\ApcCache());
+  $config->setMetadataCacheImpl(new \Doctrine\Common\Cache\ApcCache());
+
+  $serializer_builder->setCacheDir('/tmp');
+});
+
+$serializer = $serializer_builder->build();
 
 $app->add(new \PF\Slim\ResponseMiddleware($serializer));
 
 $venueDeserializer = new \PF\Serializer\VenueDeserializer();
 $venueDeserializer->setEntityManager($entityManager);
 $venueDeserializer->setSerializer($serializer);
+
+$app->view()->parserOptions = array(
+  'autoescape' => false,
+);
+
+$app->notFound(function () use ($app) {
+  $app->render('404.html');
+});
