bac9f94ff8ef0d94adf83a07bbe24da0de56f0bf
Sam T <sthompson@digitalpulp.com>
2016-03-21 23:34:52 -0400

    restore crud hints

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 
 require_once 'bootstrap.php';
 
+use JMS\Serializer\DeserializationContext;
+
 $app->group('/venue', function () use ($app, $entityManager, $serializer) {
   $app->get('/search', function () use ($app, $entityManager) {
     $venuesIterator = $entityManager->getRepository('\PF\Venue')->getVenues($app->request());
@@ -30,13 +32,23 @@ $app->group('/venue', function () use ($app, $entityManager, $serializer) {
 
     $json_venue_decoded = json_decode($json_venue_encoded, true);
 
+    $is_new_venue = empty($json_venue_encoded['id']);
+
+    $venue_serialization_context = DeserializationContext::create();
+    $venue_serialization_context->setGroups($is_new_venue ? array('create') : array('update'));
+
     $venue = $serializer->deserialize($json_venue_encoded, 'PF\Venue', 'json');
 
     if (!empty($json_venue_decoded['machines'])) {
       foreach ($json_venue_decoded['machines'] as $json_machine_decoded) {
         $json_machine_encoded = json_encode($json_machine_decoded);
 
-        $machine = $serializer->deserialize($json_machine_encoded, 'PF\Machine', 'json');
+        $is_new_machine = empty($json_machine_decoded['id']);
+
+        $machine_serialization_context = DeserializationContext::create();
+        $machine_serialization_context->setGroups($is_new_machine ? array('create') : array('update'));
+
+        $machine = $serializer->deserialize($json_machine_encoded, 'PF\Machine', 'json', $machine_serialization_context);
 
         $game = $entityManager->getRepository('\PF\Game')->find($json_machine_decoded['ipdb']);
 
@@ -50,13 +62,16 @@ $app->group('/venue', function () use ($app, $entityManager, $serializer) {
       foreach ($json_venue_decoded['comments'] as $json_comment_decoded) {
         $json_comment_encoded = json_encode($json_comment_decoded);
 
-        $comment = $serializer->deserialize($json_comment_encoded, 'PF\Comment', 'json');
+        $is_new_comment = empty($json_comment_decoded['id']);
+
+        $comment_serialization_context = DeserializationContext::create();
+        $comment_serialization_context->setGroups($is_new_comment ? array('create') : array('update'));
+
+        $comment = $serializer->deserialize($json_comment_encoded, 'PF\Comment', 'json', $comment_serialization_context);
 
         $venue->addComment($comment);
       }
     }
-
-    $is_new_venue = empty($venue->getId());
 
     try {
       $entityManager->persist($venue);
