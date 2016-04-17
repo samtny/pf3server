b6d8f611c5886f3b43eb18f13aebe03f08319383
Sam T <samtny@gmail.com>
2016-04-16 22:35:50 -0400

    test 1 bad token

--- src/samtny/fastapns/tests/FastAPNS/ClientTest.php
+++ src/samtny/fastapns/tests/FastAPNS/ClientTest.php
@@ -7,39 +7,53 @@ use FastAPNS\ClientStreamSocket;
 
 class ClientTest extends \PHPUnit_Framework_TestCase
 {
-  public function testWrite1GoodToken() {
-    $client_stream_socket = $this->getMockBuilder('ClientStreamSocket')
+  private $_client_stream_socket;
+
+  public function setUp() {
+    $this->_client_stream_socket = $this->getMockBuilder('ClientStreamSocket')
+      ->setMethods(array('isConnected', 'connect', 'write', 'read', 'status'))
       ->disableOriginalConstructor()
-      ->setMethods(array('isConnected', 'connect', 'write', 'status'))
       ->getMock();
 
-    $client_stream_socket->method('isConnected')
-      ->willReturn(FALSE);
+    $this->_client_stream_socket->method('connect')
+      ->willReturn(TRUE);
+  }
 
-    $client_stream_socket->expects($this->once())
-      ->method('isConnected');
-
-    $client_stream_socket->method('connect');
-
-    $client_stream_socket->expects($this->once())
-      ->method('connect');
-
-    $client_stream_socket->method('write')
+  public function testWrite1GoodToken() {
+    $this->_client_stream_socket->method('write')
       ->willReturn(ClientStreamSocket::FASTAPNS_WRITE_SUCCESS);
 
-    $client_stream_socket->expects($this->once())
+    $this->_client_stream_socket->expects($this->once())
       ->method('write');
 
-    $client_stream_socket->method('status')
+    $this->_client_stream_socket->method('status')
       ->willReturn(ClientStreamSocket::FASTAPNS_STATUS_OTHER);
 
-    $client_stream_socket->expects($this->once())
+    $this->_client_stream_socket->expects($this->once())
       ->method('status');
 
     $client = ClientBuilder::create()
-      ->setStreamSocketClient($client_stream_socket)
+      ->setStreamSocketClient($this->_client_stream_socket)
       ->build();
 
     $client->send('foo', array('ca360e9029938b9ed8ed435640f3760620526bd72037017d3c50cfa264b7914b'), 0);
+
+    $this->assertEmpty($client->getBadTokens());
+  }
+
+  public function testWrite1BadToken() {
+    $this->_client_stream_socket->method('write')
+      ->willReturn(ClientStreamSocket::FASTAPNS_STATUS_READABLE);
+
+    $this->_client_stream_socket->method('read')
+      ->willReturn(array('command' => 8, 'status' => 8, 'identifier' => 0));
+
+    $client = ClientBuilder::create()
+      ->setStreamSocketClient($this->_client_stream_socket)
+      ->build();
+
+    $client->send('foo', array('ca360e9029938b9ed8ed435640f3760620526bd72037017d3c50cfa264b7914b'), 0);
+
+    $this->assertEquals(1, count($client->getBadTokens()));
   }
 }
