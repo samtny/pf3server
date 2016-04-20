f2d933e7e28925ca73b0e046e486e89bfcab2535
Sam T <samtny@gmail.com>
2016-04-19 21:58:11 -0400

    token tests

--- tests/PF/Slim/Tests/LegacyVenueTest.php
+++ tests/PF/Slim/Tests/LegacyVenueTest.php
@@ -24,6 +24,44 @@ class LegacyVenueTest extends \PHPUnit_Framework_TestCase {
     $this->assertEquals(200, $response->getStatusCode());
   }
 
+  public function testLegacyCreateToken() {
+    $client = new Client(array(
+      'base_uri' => 'http://localhost:80',
+      'exceptions' => false,
+    ));
+
+    $xml = '<?xml version="1.0" encoding="UTF-8"?>';
+    $xml .= '<!DOCTYPE pinfinderapp SYSTEM "http://www.pinballfinder.org/pinfinderapp.dtd">';
+    $xml .= '<pinfinderapp><meta><user><token service="apnsfree2">&lt;abcdabce f8211498 8cd6dc4f 76ac2bab 41f4064c b165583c e702419e d1cd88db&gt;</token></user></meta></pinfinderapp>';
+
+    $response = $client->post('/legacy', array(
+      'form_params' => array(
+        'doc' => $xml,
+      ),
+    ));
+
+    $this->assertEquals(200, $response->getStatusCode());
+  }
+
+  public function testLegacyCreateVenueWithToken() {
+    $client = new Client(array(
+      'base_uri' => 'http://localhost:80',
+      'exceptions' => false,
+    ));
+
+    $xml = '<?xml version="1.0" encoding="UTF-8"?>';
+    $xml .= '<!DOCTYPE pinfinderapp SYSTEM "http://www.pinballfinder.org/pinfinderapp.dtd">';
+    $xml .= '<pinfinderapp><meta><user><token service="apnsfree2">&lt;abcdabce f8211498 8cd6dc4f 76ac2bab 41f4064c b165583c e702419e d1cd88db&gt;</token></user></meta><locations count="1"><loc><source>pinfinderfree</source><name>TEST - Modern Pinball NYC - WITH TOKEN</name><addr>362 3rd Ave</addr><city>New York</city><state>New York</state><zipcode>10016</zipcode><phone>646-415-8440</phone><lat>40.741073</lat><lon>-73.981888</lon><date>2016-01-02</date><created>2013-12-03</created><url>www.modernpinballnyc.com</url><game new="1"><abbr>P</abbr><cond>5</cond><price>0.00</price><ipdb>5938</ipdb></game><comment><ctext>01/02/16 - Game of thrones pro is in - TEST</ctext><cdate>2016-01-02 12:00:00</cdate></comment></loc></locations></pinfinderapp>';
+
+    $response = $client->post('/legacy', array(
+      'form_params' => array(
+        'doc' => $xml,
+      ),
+    ));
+
+    $this->assertEquals(200, $response->getStatusCode());
+  }
+
   public static function tearDownAfterClass() {
     $entityManager = \Bootstrap::getEntityManager();
 
