0c1cc0170058b903e8bc4d673731c607d1767844
Sam Thompson <sthompson@digitalpulp.com>
2016-04-22 11:05:20 -0400

    install composer

--- deploy.sh
+++ deploy.sh
@@ -31,7 +31,7 @@ CONFIG_FILE="./config/config.${CONFIG}.yml"
 
 eval $(parse_yaml ${CONFIG_FILE} "config_")
 
-HOST=$config_pf3server_url
+HOST=$config_pf3server_deploy_host
 USER=$config_pf3server_deploy_user
 DOCROOT=$config_pf3server_docroot
 
@@ -44,6 +44,14 @@ rsync -rv --include="bootstrap.php" --include="cli-config.php" --include="index.
 rsync -v ${CONFIG_FILE} "${USER}@${HOST}:${DOCROOT}/config.yml"
 rsync -v --ignore-existing "./credentials.yml.EXAMPLE" "${USER}@${HOST}:${DOCROOT}/credentials.yml"
 
-ssh ${USER}@${HOST} "cd $DOCROOT && composer install --no-dev --optimize-autoloader && vendor/bin/doctrine orm:generate-proxies"
+ssh ${USER}@${HOST} "mkdir -p ~/bin"
+
+ssh ${USER}@${HOST} "wget -O composer-setup.php https://getcomposer.org/installer"
+ssh ${USER}@${HOST} "php -r \"if (hash_file('SHA384', 'composer-setup.php') === '7228c001f88bee97506740ef0888240bd8a760b046ee16db8f4095c0d8d525f2367663f22a46b48d072c816e7fe19959') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;\""
+ssh ${USER}@${HOST} "php -d allow_url_fopen=On composer-setup.php"
+ssh ${USER}@${HOST} "php -r \"unlink('composer-setup.php');\""
+
+ssh ${USER}@${HOST} "php -d allow_url_fopen=On composer.phar install --working-dir=${DOCROOT} --no-dev --optimize-autoloader"
+ssh ${USER}@${HOST} "cd ${DOCROOT} && vendor/bin/doctrine orm:generate-proxies"
 
 exit 0
