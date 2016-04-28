39195212ecd2e14a6ccab608ac207ff2dafbfc3f
Sam Thompson <sthompson@digitalpulp.com>
2016-04-28 09:39:32 -0400

    fix legacy tests

--- tests/PF/Legacy/Tests/LegacyVenueTest.php
+++ tests/PF/Legacy/Tests/LegacyVenueTest.php
@@ -15,7 +15,7 @@ class LegacyTest extends \PHPUnit_Framework_TestCase {
     $xml .= '<!DOCTYPE pinfinderapp SYSTEM "http://www.pinballfinder.org/pinfinderapp.dtd">';
     $xml .= '<pinfinderapp version="2.2.2"><locations><loc><name>test venue</name><addr>Smoething</addr><city></city><state></state><zipcode></zipcode><phone></phone><url></url><game><abbr>TMNT</abbr><cond>3</cond><price>0.75</price></game><comment><ctext>Sup</ctext></comment></loc></locations></pinfinderapp>';
 
-    $response = $client->post('/legacy', array(
+    $response = $client->post('/pf2/pf', array(
       'form_params' => array(
         'doc' => $xml,
       ),
@@ -34,7 +34,7 @@ class LegacyTest extends \PHPUnit_Framework_TestCase {
     $xml .= '<!DOCTYPE pinfinderapp SYSTEM "http://www.pinballfinder.org/pinfinderapp.dtd">';
     $xml .= '<pinfinderapp><meta><user><token service="apnsfree2">&lt;abcdabce f8211498 8cd6dc4f 76ac2bab 41f4064c b165583c e702419e d1cd88db&gt;</token></user></meta></pinfinderapp>';
 
-    $response = $client->post('/legacy', array(
+    $response = $client->post('/pf2/pf', array(
       'form_params' => array(
         'doc' => $xml,
       ),
@@ -53,7 +53,7 @@ class LegacyTest extends \PHPUnit_Framework_TestCase {
     $xml .= '<!DOCTYPE pinfinderapp SYSTEM "http://www.pinballfinder.org/pinfinderapp.dtd">';
     $xml .= '<pinfinderapp><meta><user><token service="apnsfree2">&lt;abcdabce f8211498 8cd6dc4f 76ac2bab 41f4064c b165583c e702419e d1cd88db&gt;</token></user></meta><locations count="1"><loc><source>pinfinderfree</source><name>TEST - Modern Pinball NYC - WITH TOKEN</name><addr>362 3rd Ave</addr><city>New York</city><state>New York</state><zipcode>10016</zipcode><phone>646-415-8440</phone><lat>40.741073</lat><lon>-73.981888</lon><date>2016-01-02</date><created>2013-12-03</created><url>www.modernpinballnyc.com</url><game new="1"><abbr>P</abbr><cond>5</cond><price>0.00</price><ipdb>5938</ipdb></game><comment><ctext>01/02/16 - Game of thrones pro is in - TEST</ctext><cdate>2016-01-02 12:00:00</cdate></comment></loc></locations></pinfinderapp>';
 
-    $response = $client->post('/legacy', array(
+    $response = $client->post('/pf2/pf', array(
       'form_params' => array(
         'doc' => $xml,
       ),
@@ -82,7 +82,7 @@ class LegacyTest extends \PHPUnit_Framework_TestCase {
       'base_uri' => 'http://localhost:80',
     ));
 
-    $response = $client->get('/legacy?n=40.759211,-73.984638');
+    $response = $client->get('/pf2/pf?n=40.759211,-73.984638');
 
     $this->assertEquals(200, $response->getStatusCode());
 
@@ -120,7 +120,7 @@ class LegacyTest extends \PHPUnit_Framework_TestCase {
     $xml .= '<!DOCTYPE pinfinderapp SYSTEM "http://www.pinballfinder.org/pinfinderapp.dtd">';
     $xml .= '<pinfinderapp version="2.2.2"><locations><loc key="11606"><game deleted="1" /></loc></locations></pinfinderapp>';
 
-    $response = $client->post('/legacy', array(
+    $response = $client->post('/pf2/pf', array(
       'form_params' => array(
         'doc' => $xml,
       ),
@@ -135,7 +135,7 @@ class LegacyTest extends \PHPUnit_Framework_TestCase {
       'exceptions' => false,
     ));
 
-    $response = $client->get('/legacy?q=recent&t=special');
+    $response = $client->get('/pf2/pf?q=recent&t=special');
 
     $this->assertEquals(200, $response->getStatusCode());
 
@@ -159,7 +159,7 @@ class LegacyTest extends \PHPUnit_Framework_TestCase {
       'exceptions' => false,
     ));
 
-    $response = $client->get('/legacy?q=Popeye&t=game');
+    $response = $client->get('/pf2/pf?q=Popeye&t=game');
 
     $this->assertEquals(200, $response->getStatusCode());
 
@@ -201,7 +201,7 @@ class LegacyTest extends \PHPUnit_Framework_TestCase {
       'exceptions' => false,
     ));
 
-    $response = $client->get('/legacy?q=sunshine%20laundromat&t=venue');
+    $response = $client->get('/pf2/pf?q=sunshine%20laundromat&t=venue');
 
     $this->assertEquals(200, $response->getStatusCode());
 
