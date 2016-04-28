22e47cabba5900af0878f55aa64ea502af857561
Sam Thompson <sthompson@digitalpulp.com>
2016-04-28 12:52:49 -0400

    remove commented code

--- routes/legacy.php
+++ routes/legacy.php
@@ -105,7 +105,6 @@ $app->group('/pf2/pf', function () use ($app, $entityManager) {
 
     header('Cache-Control: no-cache, must-revalidate');
     header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
-    //header('Access-Control-Allow-Origin: *');
 
     $app->response->headers->set('Content-Type', 'application/xml');
     header('Content-type: application/xml');
