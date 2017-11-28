c552c382ac105f70b53399ad0ca63eb79de2983f
Sam T <samtny@gmail.com>
2017-11-28 12:03:15 -0500

    ensure sql mode

--- bootstrap.php
+++ bootstrap.php
@@ -29,6 +29,9 @@ class Bootstrap {
       'user' => $credentials['pf3server_db_user'],
       'password' => $credentials['pf3server_db_password'],
       'host' => $credentials['pf3server_db_host'],
+      'driverOptions' => array(
+        PDO::MYSQL_ATTR_INIT_COMMAND => "SET sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''))"
+      )
     );
 
     $proxy_dir = __DIR__ . '/cache';
