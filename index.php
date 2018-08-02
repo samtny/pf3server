51937d1d1be784f002f62bdc79366dd58dd53788
Sam T <samtny@gmail.com>
2018-08-02 12:49:21 -0400

    cleanup

--- index.php
+++ index.php
@@ -11,24 +11,35 @@ $config = Bootstrap::getConfig();
 
 $logger = Bootstrap::getLogger();
 
-$app = new \Slim\App(
-  [
-    'settings' => [
-      'displayErrorDetails' => true,
-      'debug' => true,
-      'determineRouteBeforeAppMiddleware' => true
-    ],
-    'response' => function () {
-      return new \PF\Slim\PinfinderResponse();
-    }
-  ]
-);
+$container = [
+  'settings' => [
+    'displayErrorDetails' => true,
+    'debug' => true,
+    'determineRouteBeforeAppMiddleware' => true
+  ],
+  'response' => function () {
+    return new \PF\Slim\PinfinderResponse();
+  }
+];
+
+$app = new \Slim\App($container);
+
+$serializer = PinfinderSerializer::create($entityManager, Bootstrap::getRunmode() === 'production');
+
+$app->add(new PinfinderResponseMiddleware($serializer));
 
 /*
 
 if (Bootstrap::getRunmode() === 'profile') {
   $app->add(new \PF\Middleware\XHProfMiddleware());
 }
+
+ */
+
+require 'routes/legacy.php';
+require 'routes/venue.php';
+
+/*
 
 require 'routes/login.php';
 
@@ -41,13 +52,7 @@ require 'routes/machine.php';
 
 require 'routes/user.php';
 require 'routes/app.php';
+
 */
-
-$serializer = PinfinderSerializer::create($entityManager, Bootstrap::getRunmode() === 'production');
-
-$app->add(new PinfinderResponseMiddleware($serializer));
-
-require 'routes/legacy.php';
-require 'routes/venue.php';
 
 $app->run();
