666ec5c451cff1bb07b80f065d9269bc24808a8f
Sam T <samtny@gmail.com>
2018-08-03 16:55:58 -0400

    WIP venue routes

--- routes/venue.php
+++ routes/venue.php
@@ -26,11 +26,57 @@ $app->group('/venue', function () use ($entityManager, $logger) {
 
     $response->setPinfinderData([
       'count' => count($venues),
-      'venues' => $venues
+      'venues' => $venues,
     ]);
 
     return $response;
 
   });
+
+  $this->get('/{id}', function ($request, $response, $args) use ($entityManager) {
+
+    $venue = $entityManager->getRepository('\PF\Venue')->find($args['id']);
+
+    if (empty($venue)) {
+      $response = $response->withStatus(404);
+    }
+    else {
+      $response->setPinfinderData([
+        'venue' => $venue,
+      ]);
+    }
+
+    return $response;
+
+  });
+
+  $this->post('/{id}/approve', function ($request, $response, $args) use ($entityManager) {
+    $venue = $entityManager->getRepository('\PF\Venue')->find($args['id']);
+
+    if (empty($venue)) {
+      $response = $response->withStatus(404);
+    }
+    else {
+      $venue->approve();
+      $entityManager->persist($venue);
+
+      if (!empty($venue->getCreatedUser())) {
+        $notification = new \PF\Notification();
+
+        $notification->setUser($venue->getCreatedUser());
+        $notification->setMessage('The venue \'' . $venue->getName() . '\' you added was approved!  Thank you!  -The Pinfinder Team');
+        $notification->setQueryParams('q=' . $venue->getId());
+
+        $entityManager->persist($notification);
+      }
+
+      $entityManager->flush();
+
+      $response->setPinfinderMessage('Approved Venue with ID ' . $venue->getId());
+    }
+
+    return $response;
+
+  })->add(new \PF\Middleware\PinfinderAdminRouteMiddleware());
 
 });
