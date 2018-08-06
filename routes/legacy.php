f9db57bbb7c514aad28a59921c9f89bbc8fbf742
Sam T <samtny@gmail.com>
2018-08-06 12:18:39 -0400

    legacy route slim3

--- routes/legacy.php
+++ routes/legacy.php
@@ -6,7 +6,7 @@ use PF\Middleware\PinfinderRequestStatsMiddleware;
 
 $app->group('/pf2/pf', function () use ($app, $entityManager, $logger) {
 
-  $app->get('', function ($request, $response) use ($app, $entityManager, $logger) {
+  $this->get('', function ($request, $response) use ($entityManager, $logger) {
 
     $legacy_request = $request;
 
@@ -132,7 +132,156 @@ $app->group('/pf2/pf', function () use ($app, $entityManager, $logger) {
 
   })->add(new PinfinderRequestStatsMiddleware());
 
-  $app->post('/gamedict/refresh', function ($request, $response, $next) use ($entityManager) {
+  $this->post('/', function ($request, $response, $args) use ($entityManager) {
+    $xml = $request->getParsedBodyParam('doc');
+
+    $legacy_request = new PF\Legacy\Request();
+
+    $legacy_request->loadXML($xml);
+
+    $user = NULL;
+
+    if (!empty($legacy_request->user)) {
+      $legacy_user = $legacy_request->user;
+
+      if (!empty($legacy_user->tokens)) {
+        $legacy_token = $legacy_user->tokens[0];
+
+        $legacy_service = $legacy_token->service;
+        $legacy_token_string = $legacy_token->token;
+
+        if (!empty($legacy_service) && !empty($legacy_token_string)) {
+          $tokenApp = $legacy_service;
+          $tokenString = preg_replace('/\s|<|>/', '', $legacy_token_string);
+
+          switch ($tokenApp) {
+            case 'apns':
+            case 'apnsfree':
+            case 'apnsfree2':
+              $tokenApp = 'apnsfree';
+
+              break;
+            default:
+              $tokenApp = 'apnspro';
+
+              break;
+          }
+
+          $token = $entityManager->getRepository('PF\Token')->findOneBy(array('app' => $tokenApp, 'token' => $tokenString));
+
+          if (empty($token)) {
+            $token = new PF\Token();
+
+            $token->setApp($tokenApp);
+            $token->setToken($tokenString);
+
+            $user = new PF\User();
+
+            $user->addToken($token);
+
+            $entityManager->persist($user);
+          } else {
+            $user = $token->getUser();
+          }
+        }
+      }
+    }
+
+    foreach ($legacy_request->venues as $legacy_venue) {
+      $is_new_venue = empty($legacy_venue->id);
+
+      $venue = NULL;
+
+      if ($is_new_venue) {
+        $venue = new \PF\Venue();
+
+        $venue->setCreatedUser($user);
+      } else {
+        $venue = $entityManager->find('PF\Venue', $legacy_venue->id);
+      }
+
+      if (!empty($venue)) {
+        $venue->touch();
+
+        !empty($legacy_venue->name) && $venue->setName($legacy_venue->name);
+        !empty($legacy_venue->street) && $venue->setStreet($legacy_venue->street);
+        !empty($legacy_venue->city) && $venue->setCity($legacy_venue->city);
+        !empty($legacy_venue->state) && $venue->setState($legacy_venue->state);
+        !empty($legacy_venue->zipcode) && $venue->setZipcode($legacy_venue->zipcode);
+        !empty($legacy_venue->lat) && $venue->setLatitude($legacy_venue->lat);
+        !empty($legacy_venue->lon) && $venue->setLongitude($legacy_venue->lon);
+        !empty($legacy_venue->phone) && $venue->setPhone($legacy_venue->phone);
+        !empty($legacy_venue->url) && $venue->setUrl($legacy_venue->url);
+
+        foreach ($legacy_venue->games as $legacy_machine) {
+          $machine = NULL;
+
+          if (!empty($legacy_machine->id)) {
+            $machine = $entityManager->find('PF\Machine', $legacy_machine->id);
+
+            !empty($legacy_machine->cond) && $machine->setCondition($legacy_machine->cond);
+            !empty($legacy_machine->price) && $machine->setPrice($legacy_machine->price);
+
+            if ($legacy_machine->deleted) {
+              $entityManager->remove($machine);
+            } else {
+              $entityManager->persist($machine);
+            }
+          } else {
+            $game = $entityManager->getRepository('\PF\Game')->findOneBy(array('abbreviation' => $legacy_machine->abbr));
+
+            if (!empty($game)) {
+              $machine = new \PF\Machine();
+
+              $machine->setCondition($legacy_machine->cond);
+              $machine->setPrice($legacy_machine->price);
+              $machine->setGame($game);
+
+              $venue->addMachine($machine);
+            }
+          }
+        }
+
+        foreach ($legacy_venue->comments as $legacy_comment) {
+          if (empty($legacy_comment->id)) {
+            $comment = new \PF\Comment();
+
+            $comment->setText($legacy_comment->text);
+
+            $venue->addComment($comment);
+          }
+        }
+
+        if (!empty($legacy_venue->flag)) {
+          $venue->flag();
+        }
+
+        $entityManager->persist($venue);
+      }
+    }
+
+    $entityManager->flush();
+
+    $legacy_result = new PF\Legacy\Result();
+
+    $legacy_status = new PF\Legacy\Status();
+    $legacy_status->status = 'success';
+
+    $legacy_result->status = $legacy_status;
+
+    header('HTTP/1.1 200 OK');
+
+    $legacy_result_xml = $legacy_result->saveXML();
+
+    header('Content-Length: ' . strlen($legacy_result_xml));
+    header('Content-Type: application/xml;type=result;charset="utf-8"');
+
+    echo $legacy_result_xml;
+
+    return $response->withStatus(200);
+  });
+
+  $this->post('/gamedict/refresh', function ($request, $response, $next) use ($entityManager) {
 
     $params = [
       'l' => 999999,
